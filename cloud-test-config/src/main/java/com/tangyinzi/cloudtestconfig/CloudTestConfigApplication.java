777b4e472cb52f0d613316eb59cb5b42c432cfcd
linjiajie <linjiajie@weizhi.com>
2022-05-09 17:07:13 +0800

    update

--- cloud-test-config/src/main/java/com/tangyinzi/cloudtestconfig/CloudTestConfigApplication.java
+++ cloud-test-config/src/main/java/com/tangyinzi/cloudtestconfig/CloudTestConfigApplication.java
@@ -3,10 +3,12 @@ package com.tangyinzi.cloudtestconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class CloudTestConfigApplication {
 
     public static void main(String[] args) {
--- cloud-test-service/src/main/java/com/tangyinzi/cloudtestservice/CloudTestServiceApplication.java
+++ cloud-test-service/src/main/java/com/tangyinzi/cloudtestservice/CloudTestServiceApplication.java
@@ -1,11 +1,14 @@
 package com.tangyinzi.cloudtestservice;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ApplicationContext;
 
 /**
  * @author 高处不胜寒
@@ -14,6 +17,13 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = {"com.tangyinzi.cloudtestservice.client"})
 public class CloudTestServiceApplication {
+	private static final Logger logger = LoggerFactory.getLogger(CloudTestServiceApplication.class);
+
+	public CloudTestServiceApplication(ApplicationContext ctx){
+
+		logger.info("激活的配置(mytest): {}", ctx.getEnvironment().getProperty("mytest"));
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudTestServiceApplication.class, args);
